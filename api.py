d6c5b33a17115122c2d2fee1fff3174878305904
lee_jiazh <lee_jiazh@163.com>
2018-02-05 00:06:21 +0800

    add cos sim and test

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from aip import AipNlp
+import numpy as np
 
 APP_ID = '10791703'
 API_KEY = 'YNMY6eSAz6QlDCeM7KB9GKHC'
@@ -9,7 +10,18 @@ client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
 
 def get_wordEmbedding(word):
     we = client.wordEmbedding(word)
-    return we if 'vec' in we else None
+    return np.array(we['vec']) if 'vec' in we else None
+
+def module(vec):
+    from functools import reduce
+    import math
+    return math.sqrt( reduce(lambda x, y: x+math.pow(y, 2), vec, 0))
+
+def get_wordSimilarity(word1, word2):
+    w1, w2 = get_wordEmbedding(word1), get_wordEmbedding(word2)
+    if w1 is None or w2 is None: return None
+    return float(np.dot(w1, w2)) / (module(w1) * module(w2))
+    
 
 def get_lexer(text):
     return client.lexer(text)
--- api_test.py
+++ api_test.py
@@ -4,10 +4,23 @@ from api import *
 class TestReader(unittest.TestCase):
     def test_wordEmbedding(self):
         self.assertEqual(get_wordEmbedding('张飞哈哈'), None)
-        self.assertEqual('vec' in get_wordEmbedding('张飞'), True)
+        self.assertEqual(get_wordEmbedding('张飞').shape, (1024,))
 
     def test_lexer(self):
         print(get_lexer("我是你爸爸！"))
+
+    def test_module(self):
+        self.assertEqual(module(np.array([3, 4])), 5)
+        
+    def test_cos_similarity(self):
+        print(get_wordSimilarity("big", "large"))   # 0.19
+        print(get_wordSimilarity("字体", "宋体"))   # 0.22
+        print(get_wordSimilarity("中国", "北京"))   #0.16
+        print(get_wordSimilarity("父亲", "儿子"))   #0.40
+        #self.assertTrue(get_wordSimilarity("big", "large") > 0.8)
+        #self.assertTrue(get_wordSimilarity("字体", "宋体") > 0.7)
+        #self.assertTrue(get_wordSimilarity("颜色", "绿色") > 0.6)
+
 
 if __name__ == "__main__":
     unittest.main()
