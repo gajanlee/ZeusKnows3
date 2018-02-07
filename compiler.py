9309317aea07abffa667e37a1661aa5c87ee22ff
lee_jiazh <lee_jiazh@163.com>
2018-02-07 23:14:10 +0800

    add some rule

--- compiler.py
+++ compiler.py
@@ -1,15 +1,93 @@
 # compiler.py
 
+postag_dict = {
+    'Ag':'形语素',
+    'g':'语素',
+    'ns':'地名',
+    'u':'助词',
+    'a':'形容词',
+    'h':'前接成分',
+    'nt':'机构团体',
+    'vg':'动语素',
+    'ad':'副形词',
+    'i':'成语',
+    'nz':'其他专名',
+    'v':'动词',
+    'an':'名形词',
+    'j':'简称略语',
+    'o':'拟声词',
+    'vd':'副动词',
+    'b':'区别词',
+    'k':'后接成分',
+    'p':'介词',
+    'vn':'名动词',
+    'c':'连词',
+    'l':'习用语',
+    'q':'量词',
+    'w':'标点符号',
+    'dg':'副语素',
+    'm':'数词',
+    'r':'代词',
+    'x':'非语素字',
+    'd':'副词',
+    'Ng':'名语素',
+    's':'处所词',
+    'y':'语气词',
+    'e':'叹词',
+    'n':'名词',
+    'tg':'时语素',
+    'z':'状态词',
+    'f':'方位词',
+    'nr':'人名',
+    't':'时间词',
+    'un':'未知词',}
+
 class Tripe_Director:
     def __init__(self, word_list):
-        self.word_list = self.convert_word(word_list)
+        self.token_list = [Token(word) for word in word_list]
     
+    def graph(self):
+        a = []
+        for token in self.token_list:
+            try:
+                a.append(getattr(self, token.deprel, None)(token, self.HEAD(token)))
+            except:
+                raise CompilerDepError("No this deprelfunc " + token.deprel)
+    
+    def HEAD(self, token):
+        if token.head == -1: return None
+        return self.token_list[token.head]
+
+    def SBV(self, token1, token2):
+        return (token1, token2)
+    
+    """
+    ATT:定中关系,应当合并为一个词语。
+    :param token*: token1 -> token2,
+                 and token2
+    """
+    def ATT(self, token1, token2):
+        return token1.merge(token2)
+
+    """
+    FOB:前置定语，Head方向变换。
+    """
+    def FOB(self, token1, token2):
+        return (token2, token1)
+
+    def HED(self, token1, token2):
+        pass
+
+    def ADV(self, token1, token2):
+        return token1.merge(token2)
+
+    def WP(self, token1, token2):
+        pass
     """
     Convert HTTP Api word list to 'Token' instance list.
     """
     def convert_word(self, word_list):
-        pass
-
+        return [Token(word) for word in word_list]
 
 class Tripe:
     """
@@ -21,7 +99,9 @@ class Tripe:
         (Linus, Linux, father) is the meaning of "Linus is the father of Linux".
     """
     def __init__(self, token1, token2, relationship):
-        pass
+        self.token1 = token1
+        self.token2 = token2
+        self.relationship = relationship
 
 class Token:
     """
@@ -29,9 +109,20 @@ class Token:
     :param token: is an element of word list.
     """
     def __init__(self, token):
-        pass
+        self.postag = token['postag']
+        self.head = token['head'] - 1   # if it is -1, it will be non-head word.
+        self.content = token['word']
+        self.deprel = token['deprel']
 
     def __repr__(self):
-        pass
+        return postag_dict[self.postag] + '\t' + self.content
 
+    def merge(self, token):
+        self.content += token.content
 
+class CompilerDepError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
